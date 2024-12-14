3fb952d2c3b0d4533d426a1ff15de90f11a4e294
JhonierM14 <144240957+JhonierM14@users.noreply.github.com>
2024-12-13 23:44:38 -0500

    commit registroprof

--- frontend/src/pages/Home/Registro/Registro.js
+++ frontend/src/pages/Home/Registro/Registro.js
@@ -1,51 +1,3 @@
-<<<<<<< HEAD
-import React, { useState} from 'react';
-import { Link } from "react-router-dom";
-import "./RegistroEst.css";
-import {createUsuario } from '../../../api/usuarioApi';
-
-
-const RegistroEst = () => {
-  const [formData, setFormData] = useState({
-    id_rol: 3, // Rol de Estudiante por defecto
-    nombre: "",
-    apellido: "",
-    correo: "",
-    contraseña: "",
-    confirmarContraseña: "",
-    telefono: "",
-    foto_registro: "",// Aquí se ingresará la URL de la fotografía
-    codigo: "", 
-  });
-  
-   // Manejar cambios en los inputs
-   const handleChange = (e) => {
-    const { id, value } = e.target;
-    setFormData({
-      ...formData,
-      [id]: value,
-    });
-  };
-
-  // Manejar el envío del formulario
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const { confirmarContraseña, ...dataToSend } = formData;
-
-    if (formData.contraseña !== confirmarContraseña) {
-      alert("Las contraseñas no coinciden");
-      return;
-    }
-
-    try {
-      await createUsuario(dataToSend); // Llamada a la API
-      alert("Usuario registrado exitosamente");
-    } catch (error) {
-      console.error("Error al registrar usuario:", error);
-      alert("Hubo un error al registrar el usuario");
-    }
-  };
-=======
 import React, { useState } from "react";
 import { Link } from "react-router-dom"; 
 import { createUsuario } from "../../../api/usuarioApi";
@@ -98,32 +50,20 @@ const Registro = () => {
   }
   };
   
->>>>>>> 024d2a78575af5775bfcc56d9a8835f72e5b45c4
 
   return (
     <div className="registro-container">
       <div className="form-container">
         <h1 className="logo">Attenzio</h1>
-<<<<<<< HEAD
-        <form onSubmit={handleSubmit} className="usuario-form">
-          <div className="form-row">
-          <div className="form-group">
-=======
         <form onSubmit={handleSubmit}>
           <div className="form-row">
             <div className="form-group">
->>>>>>> 024d2a78575af5775bfcc56d9a8835f72e5b45c4
               <label htmlFor="nombre">Nombre</label>
               <input
                 type="text"
                 id="nombre"
-<<<<<<< HEAD
-                value={formData.nombre}
-                onChange={handleChange}
-=======
                 value={nombre}
                 onChange={(e) => setNombre(e.target.value)}
->>>>>>> 024d2a78575af5775bfcc56d9a8835f72e5b45c4
                 placeholder="Ingrese su nombre"
               />
             </div>
@@ -132,13 +72,8 @@ const Registro = () => {
               <input
                 type="text"
                 id="apellido"
-<<<<<<< HEAD
-                value={formData.apellido}
-                onChange={handleChange}
-=======
                 value={apellido}
                 onChange={(e) => setApellido(e.target.value)}
->>>>>>> 024d2a78575af5775bfcc56d9a8835f72e5b45c4
                 placeholder="Ingrese sus apellidos"
               />
             </div>
@@ -149,26 +84,12 @@ const Registro = () => {
               <input
                 type="email"
                 id="correo"
-<<<<<<< HEAD
-                value={formData.correo}
-                onChange={handleChange}
-=======
                 value={correo}
                 onChange={(e) => setCorreo(e.target.value)}
->>>>>>> 024d2a78575af5775bfcc56d9a8835f72e5b45c4
                 placeholder="Ingrese su correo"
               />
             </div>
             <div className="form-group">
-<<<<<<< HEAD
-              <label htmlFor="telefono">Telefono</label>
-              <input 
-              type="text" 
-              id="telefono" 
-              value={formData.telefono}
-              onChange={handleChange}
-              placeholder="Ingrese su teléfono" 
-=======
               <label htmlFor="cedula">Cédula</label>
               <input
                 type="text"
@@ -176,21 +97,11 @@ const Registro = () => {
                 value={cedula}
                 onChange={(e) => setCedula(e.target.value)}
                 placeholder="Ingrese su cédula"
->>>>>>> 024d2a78575af5775bfcc56d9a8835f72e5b45c4
               />
             </div>
           </div>
           <div className="form-row">
             <div className="form-group">
-<<<<<<< HEAD
-              <label htmlFor="foto_registro">Foto matricula</label>
-              <input 
-              type="text" 
-              id="foto_registro"
-              value={formData.foto_registro}
-              onChange={handleChange}
-              placeholder="Ingrese el link de drive"             
-=======
               <label htmlFor="direccion">Dirección</label>
               <input
                 type="text"
@@ -208,23 +119,10 @@ const Registro = () => {
                 value={foto}
                 onChange={(e) => setFoto(e.target.value)}
                 placeholder="Ingrese la URL de la fotografía"
->>>>>>> 024d2a78575af5775bfcc56d9a8835f72e5b45c4
               />
             </div>
           </div>
           <div className="form-row">
-<<<<<<< HEAD
-            <div className="form-group">
-              <label htmlFor="codigo">Código</label>
-              <input 
-              type="text" 
-              id="codigo"
-              value={formData.codigo}
-              onChange={handleChange}
-              placeholder="Ingrese su código de estudiante" 
-              />
-            </div>
-=======
             <label htmlFor="esMonitor">Monitor</label>
             <input 
             type="checkbox" 
@@ -233,7 +131,6 @@ const Registro = () => {
             value={true}
             onClick={(e) => setMonitor(e.target.value)}
             />  
->>>>>>> 024d2a78575af5775bfcc56d9a8835f72e5b45c4
           </div>
           <div className="form-row">
             <div className="form-group">
@@ -241,13 +138,8 @@ const Registro = () => {
               <input
                 type="password"
                 id="contraseña"
-<<<<<<< HEAD
-                value={formData.contraseña}
-                onChange={handleChange}
-=======
                 value={contraseña}
                 onChange={(e) => setContraseña(e.target.value)}
->>>>>>> 024d2a78575af5775bfcc56d9a8835f72e5b45c4
                 placeholder="Ingrese su contraseña"
               />
             </div>
@@ -256,42 +148,22 @@ const Registro = () => {
               <input
                 type="password"
                 id="confirmarContraseña"
-<<<<<<< HEAD
-                value={formData.confirmarContraseña}
-                onChange={handleChange}
-=======
                 value={confirmarContraseña}
                 onChange={(e) => setConfirmarContraseña(e.target.value)}
->>>>>>> 024d2a78575af5775bfcc56d9a8835f72e5b45c4
                 placeholder="Confirme su contraseña"
               />
             </div>
           </div>
-<<<<<<< HEAD
-          <div className="form-group">
-            <button type="submit" className="register-button">Registrarse</button>
-            <button className="home-button"><Link to="/">Inicio</Link></button>
-          </div>
-=======
           
             <div className="form-group">
               <button type="submit" className="register-button">Registrarse</button>
               <button className="home-button"><Link to="/">Inicio</Link></button>
             </div>
->>>>>>> 024d2a78575af5775bfcc56d9a8835f72e5b45c4
           
         </form>
       </div>
     </div>
   );
-<<<<<<< HEAD
-}
-
-
-
-export default RegistroEst;
-=======
 };
 
 export default Registro;
->>>>>>> 024d2a78575af5775bfcc56d9a8835f72e5b45c4
